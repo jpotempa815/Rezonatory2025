e06aed02fe1cac55e8ad1930fe7c05519f7f18e0
jpotempa815 <julia.potempa13@gmail.com>
2025-08-18 16:49:34 +0200

    pomiary 180825

--- skrypty/transmittance_vs_position.py
+++ skrypty/transmittance_vs_position.py
@@ -4,16 +4,16 @@ import pandas as pd
 import os
 
 # wpisz ścieżkę do katalogu z danymi
-my_path = r'C:\Users\mkowa\Desktop\Julia\Rezonatory2025\pomiary\pomiary_140825'
-file1 = r'swcnt_sa-3(2)_1_140825.csv'
-file2 = r'swcnt_sa-3(2)_2_140825.csv' 
-file3 = r'swcnt_sa-3(2)_4_140825.csv'
-file4 = r'grafen_b2_szklo_140825.csv'
+my_path = r'C:\Users\mkowa\Desktop\Julia\Rezonatory2025\pomiary\pomiary_180825'
+file1 = r'SixCarbon_probka_1_180825.csv'
+file2 = r'SixCarbon_probka_2_180825.csv' 
+file3 = r'SixCarbon_probka_3_180825.csv'
+file4 = r'SixCarbon_szklo_180825.csv'
 file5 = r'C:\Users\mkowa\Desktop\Julia\Rezonatory2025\pomiary\pomiary_130825\1_grafen_m1_szklo_130825.csv'
 # nazwa katalogu zapisu
-save_path = r'C:\Users\mkowa\Desktop\Julia\Rezonatory2025\wyniki\wyniki_140825'
+save_path = r'C:\Users\mkowa\Desktop\Julia\Rezonatory2025\wyniki\wyniki_180825'
 # wpisz nazwę pliku do zapisu wykresus
-file_save = r'swcnt_sa-3(2)_plot.png'
+file_save = r'SixCarbon_plot.png'
 
 plt.figure(figsize=(8,6))
 
@@ -41,13 +41,13 @@ P2_wsp = P2/P_ref
 
 P_strat = 1 + (1 - P2_wsp) #czynnik strat (czyli ile więcej transmitancji ostatecznie jest)
 
-# P1_1 = df1['Power [W]']/P_ref *P_strat * 100
-# P1_2 = df2['Power [W]']/P_ref * P_strat * 100
-# P1_3 = df3['Power [W]']/P_ref * P_strat * 100
-P1_1 = df1['Power [W]']/P_ref * 100
-P1_2 = df2['Power [W]']/P_ref * 100
-P1_3 = df3['Power [W]']/P_ref * 100
-P2 = df4['Power [W]']/P_ref * 100
+P1_1 = df1['Power [W]']/P_ref *P_strat * 100
+P1_2 = df2['Power [W]']/P_ref * P_strat * 100
+P1_3 = df3['Power [W]']/P_ref * P_strat * 100
+# P1_1 = df1['Power [W]']/P_ref * 100
+# P1_2 = df2['Power [W]']/P_ref * 100
+# P1_3 = df3['Power [W]']/P_ref * 100
+# P2 = df4['Power [W]']/P_ref * 100
 # P2 = P2 / P_ref *100
 
 z1 = df1['Position [mm]']
@@ -57,7 +57,7 @@ z1 = df1['Position [mm]']
 plt.plot(z1, P1_1, color='darkorange', label = 'Punkt 1')
 plt.plot(z1, P1_2, color='red', label = 'Punkt 2')
 plt.plot(z1, P1_3, color='green', label = 'Punkt 3')
-plt.plot(z1, P2, color='blue', label = 'Szkło')
+# plt.plot(z1, P2, color='blue', label = 'Szkło 1')
 plt.xlabel('Pozycja [mm]')
 # plt.ylabel('Moc [W]')
 plt.ylabel('Transmitancja [%]')
@@ -65,5 +65,5 @@ plt.ylabel('Transmitancja [%]')
 plt.xlim(0,13)
 plt.legend()
 plt.grid(linestyle='--')
-# plt.savefig(os.path.join(save_path, file_save))
+plt.savefig(os.path.join(save_path, file_save))
 plt.show()
